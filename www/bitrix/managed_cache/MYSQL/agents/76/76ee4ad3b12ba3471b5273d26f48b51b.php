46093cc636734f5f0eb4857306264aef187e8071
axelwarn2 <722shock722@mail.ru>
2024-10-21 12:05:09 +0500

    Добавит ядро в игнор

---
+++ www/bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
@@ -0,0 +1,7 @@
+<?
+if($INCLUDE_FROM_CACHE!='Y')return false;
+$datecreate = '001729493915';
+$dateexpire = '001729497515';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1729493936;}';
+return true;
+?>
